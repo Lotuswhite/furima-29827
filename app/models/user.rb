d68106fe2ee5f7412e53cae40f4f816df2f2036c
Lotuswhite <renisawa0105@icloud.com>
2020-09-28 18:58:30 +0900

    モデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
  validates :nickname, presence: true
+
+ PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+ validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+  validates :family_name
+  validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン一]+\z/ } do
+    validates :family_name_kana
+    validates :first_name_kana
+    end
+
+  validates :birthday, presence: true
 end
