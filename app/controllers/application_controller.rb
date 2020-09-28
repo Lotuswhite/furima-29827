174b5b595df8867841d292e5f53e413ca9ca7c8c
Lotuswhite <renisawa0105@icloud.com>
2020-09-28 19:11:26 +0900

    rubocop実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,11 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-     username == "LOUTS" && password == "0105"
+      username == 'LOUTS' && password == '0105'
     end
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday])
-      
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
- validates :nickname, presence: true
+  validates :nickname, presence: true
 
- PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
- validates_format_of :password, with: PASSWORD_REGEX
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-  validates :family_name
-  validates :first_name
+    validates :family_name
+    validates :first_name
   end
 
   with_options presence: true, format: { with: /\A[ァ-ン一]+\z/ } do
     validates :family_name_kana
     validates :first_name_kana
-    end
+  end
 
   validates :birthday, presence: true
 end
