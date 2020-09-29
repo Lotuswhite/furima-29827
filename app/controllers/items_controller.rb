4e9b16a515cbdb764d346bccc46f4231cab87036
Lotuswhite <renisawa0105@icloud.com>
2020-09-29 16:11:33 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,11 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
