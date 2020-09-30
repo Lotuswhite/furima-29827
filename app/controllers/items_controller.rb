90e4537b9a45660e08c79684e661e7c6bdae9908
Lotuswhite <renisawa0105@icloud.com>
2020-09-30 13:09:10 +0900

    出品可能になった

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,12 +3,23 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
-  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
   private
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:name, :info, :image, :category_id, :status_id, :shopping_status_id, :prifecture_id, :scheduled_id, :price ).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+  belongs_to_active_hash :area
+  belongs_to_active_hash :delivery
+  belongs_to_active_hash :day
+  belongs_to_active_hash :state
+
+
   belongs_to :user
   has_one_attached :image
+
 end
