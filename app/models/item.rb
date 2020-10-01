c91e785e443fb9e89f450614f7cb1a27c7e8774f
Lotuswhite <renisawa0105@icloud.com>
2020-10-01 19:18:31 +0900

    修正3

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,14 +6,12 @@ class Item < ApplicationRecord
   belongs_to_active_hash :day
   belongs_to_active_hash :state
 
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :info, presence: true
+  validates :image, :name, :info, presence: true
 
   with_options presence: true, format: { with: /\A[a-z0-9]+\z/i.freeze } do
     validates :price, inclusion: { in: 300..9_999_999 }
   end
-  
+
   validates :category_id,:status_id,:shopping_status_id,:prifecture_id,:scheduled_id, numericality: { other_than: 0 }
   
   belongs_to :user
